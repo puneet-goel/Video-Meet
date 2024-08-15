bfc194c8c9d6c0dcc9817961d2e431f516afd801
Puneet Goel <puneetgoel016@gmail.com>
2024-08-15 16:11:41 +0530

    functions fix

--- client/src/components/Room/Room.jsx
+++ client/src/components/Room/Room.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 import { useHistory } from 'react-router-dom'
 import { ToastContainer, toast } from 'react-toastify'
 import Peer from 'simple-peer'
@@ -127,147 +127,153 @@ const Room = (props) => {
     setMessage('')
   }
 
-  useEffect(() => {
-    const createPeer = (receiver, senderName, sender, stream) => {
-      const peer = new Peer({
-        initiator: true,
-        trickle: false,
-        stream
+  const createPeer = (receiver, senderName, sender, stream) => {
+    const peer = new Peer({
+      initiator: true,
+      trickle: false,
+      stream
+    })
+    peer.on('signal', (signal) => {
+      socket.current.emit('sending signal', {
+        receiver,
+        senderName,
+        signal,
+        sender
       })
-      peer.on('signal', (signal) => {
-        socket.current.emit('sending signal', {
-          receiver,
-          senderName,
-          signal,
-          sender
-        })
+    })
+    return peer
+  }
+
+  const addPeer = (incomingSignal, sender, stream) => {
+    const peer = new Peer({
+      initiator: false,
+      trickle: false,
+      stream
+    })
+
+    peer.on('signal', (signal) => {
+      socket.current.emit('returning signal', { signal, sender })
+    })
+    peer.signal(incomingSignal)
+    return peer
+  }
+
+  const socketFunctions = useCallback(async () => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: true
       })
-      return peer
-    }
 
-    const addPeer = (incomingSignal, sender, stream) => {
-      const peer = new Peer({
-        initiator: false,
-        trickle: false,
-        stream
+      myVideo.current.srcObject = stream
+
+      //syncing video and audio with landing page(AskPermission)
+      if (myVideo.current.srcObject) {
+        if (myVideo.current.srcObject.getVideoTracks().length > 0) {
+          myVideo.current.srcObject.getVideoTracks()[0].enabled =
+            sessionStorage.getItem('video') === 'true'
+        }
+        if (myVideo.current.srcObject.getAudioTracks().length > 0) {
+          myVideo.current.srcObject.getAudioTracks()[0].enabled =
+            sessionStorage.getItem('audio') === 'true'
+        }
+      }
+
+      socket.current.emit('join room', roomID, myName.current)
+
+      socket.current.on('room full', () => {
+        alert('Room full!')
+        history.push('/')
       })
 
-      peer.on('signal', (signal) => {
-        socket.current.emit('returning signal', { signal, sender })
-      })
-      peer.signal(incomingSignal)
-      return peer
-    }
-
-    navigator.mediaDevices
-      .getUserMedia({ video: true, audio: true })
-      .then((stream) => {
-        myVideo.current.srcObject = stream
-
-        //syncing video and audio with landing page(AskPermission)
-        if (myVideo.current.srcObject) {
-          if (myVideo.current.srcObject.getVideoTracks().length > 0) {
-            myVideo.current.srcObject.getVideoTracks()[0].enabled =
-              sessionStorage.getItem('video') === 'true'
+      socket.current.on('allExceptMe', (users) => {
+        const peers = []
+        //receiver[0] = id, receiver[1] =name
+        users.forEach((receiver) => {
+          const peer = createPeer(
+            receiver[0],
+            myName.current,
+            socket.current.id,
+            stream
+          )
+          const x = {
+            peerID: receiver[0],
+            peerName: receiver[1],
+            peer
           }
-          if (myVideo.current.srcObject.getAudioTracks().length > 0) {
-            myVideo.current.srcObject.getAudioTracks()[0].enabled =
-              sessionStorage.getItem('audio') === 'true'
+
+          addParticipant(receiver[0], receiver[1])
+          peersRef.current.push(x)
+          peers.push(x)
+        })
+        setPeers(peers)
+      })
+
+      socket.current.on('user-joined', (data) => {
+        const peer = addPeer(data.signal, data.sender, stream)
+        const x = {
+          peerID: data.sender,
+          peerName: data.senderName,
+          peer
+        }
+        addParticipant(data.sender, data.senderName)
+        peersRef.current.push(x)
+        setPeers([...peersRef.current])
+      })
+
+      socket.current.on('user-left', (id) => {
+        const item = peersRef.current.find((p) => p.peerID === id)
+        item.peer.destroy()
+        const x = peersRef.current.filter((p) => p.peerID !== id)
+        peersRef.current = x
+        removeParticipant(id)
+        setPeers(x)
+      })
+
+      socket.current.on('receiving returned signal', (data) => {
+        const item = peersRef.current.find((p) => p.peerID === data.receiver)
+        item.peer.signal(data.signal)
+      })
+
+      socket.current.on('incoming message', (data) => {
+        let dir = 'start'
+        if (data.sender === socket.current.id) {
+          dir = 'end'
+        }
+
+        if (data.type === 'user-join' || data.type === 'user-left') {
+          dir = 'center'
+
+          if (data.sender !== socket.current.id) {
+            toast(`${data.message}`, {
+              position: 'bottom-left',
+              autoClose: 1500,
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+              theme: 'dark'
+            })
           }
         }
 
-        socket.current.emit('join room', roomID, myName.current)
-
-        socket.current.on('room full', () => {
-          alert('Room full!')
-          history.push('/')
-        })
-
-        socket.current.on('allExceptMe', (users) => {
-          const peers = []
-          //receiver[0] = id, receiver[1] =name
-          users.forEach((receiver) => {
-            const peer = createPeer(
-              receiver[0],
-              myName.current,
-              socket.current.id,
-              stream
-            )
-            const x = {
-              peerID: receiver[0],
-              peerName: receiver[1],
-              peer
-            }
-
-            addParticipant(receiver[0], receiver[1])
-            peersRef.current.push(x)
-            peers.push(x)
-          })
-          setPeers(peers)
-        })
-
-        socket.current.on('user-joined', (data) => {
-          const peer = addPeer(data.signal, data.sender, stream)
-          const x = {
-            peerID: data.sender,
-            peerName: data.senderName,
-            peer
-          }
-          addParticipant(data.sender, data.senderName)
-          peersRef.current.push(x)
-          setPeers([...peersRef.current])
-        })
-
-        socket.current.on('user-left', (id) => {
-          const item = peersRef.current.find((p) => p.peerID === id)
-          item.peer.destroy()
-          const x = peersRef.current.filter((p) => p.peerID !== id)
-          peersRef.current = x
-          removeParticipant(id)
-          setPeers(x)
-        })
-
-        socket.current.on('receiving returned signal', (data) => {
-          const item = peersRef.current.find((p) => p.peerID === data.receiver)
-          item.peer.signal(data.signal)
-        })
-
-        socket.current.on('incoming message', (data) => {
-          let dir = 'start'
-          if (data.sender === socket.current.id) {
-            dir = 'end'
-          }
-
-          if (data.type === 'user-join' || data.type === 'user-left') {
-            dir = 'center'
-
-            if (data.sender !== socket.current.id) {
-              toast(`${data.message}`, {
-                position: 'bottom-left',
-                autoClose: 1500,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-                theme: 'dark'
-              })
-            }
-          }
-
-          const x = {
-            id: uuid(),
-            message: data.message,
-            name: data.name,
-            dir: dir,
-            time: data.time
-          }
-          setChat((cur) => [...cur, x])
-        })
+        const x = {
+          id: uuid(),
+          message: data.message,
+          name: data.name,
+          dir: dir,
+          time: data.time
+        }
+        setChat((cur) => [...cur, x])
       })
-      .catch((err) => {
-        console.log(err)
-      })
+    } catch (err) {
+      console.log(err)
+    }
+  }, [history, roomID])
+
+  useEffect(() => {
+    socketFunctions()
 
     //socket instance deleted, when user clicks back button in history
     const remove = () => {
@@ -277,7 +283,7 @@ const Room = (props) => {
     return () => {
       window.removeEventListener('popstate', remove)
     }
-  }, [history, roomID])
+  }, [socketFunctions])
 
   return (
     <div className='container-fluid p-0'>
